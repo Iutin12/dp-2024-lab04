e23a81891e91ff04491cf742fc97d9fb1701681d
Iutin12 <79744000+Iutin12@users.noreply.github.com>
2024-11-06 22:45:05 +0500

    docs: добавлены константы

--- clocks/clock_adapter.py
+++ clocks/clock_adapter.py
@@ -3,74 +3,44 @@ from consts.date_consts import DayNightDivision
 from interfaces.base_analog_clock import BaseAnalogClock
 from interfaces.base_digital_clock import BaseDigitalClock
 from consts.translate import ClockConstants
+from clocks.helper import ClockTimeHelper
 
 class AnalogClockAdapter(BaseDigitalClock):
     def __init__(self, analog_clock: BaseAnalogClock) -> None:
         """
-        Инициализирует адаптер для аналоговых часов.
+        Инициализирует адаптер для аналоговых часов
 
-        analog_clock - Экземпляр класса, реализующего интерфейс BaseAnalogClock
+        :param analog_clock: Экземпляр класса, реализующего интерфейс BaseAnalogClock, который будет адаптирован для использования как цифровые часы
         """
-        self.analog_clock = analog_clock
+        self._analog_clock = analog_clock
 
     def set_date_time(self, date: datetime) -> None:
         """
-        Устанавливает дату и время на аналоговых часах
+        Устанавливает дату и время на аналоговых часах, преобразуя их в углы для
+        часов, минут и секунд, а также определяя AM/PM для правильного отображения
 
-        date - Дата и время в формате datetime
+        :param date: Дата и время, которые нужно установить на аналоговых часах
         """
-
         year, month, day = date.year, date.month, date.day
-        hour_angle, minute_angle, second_angle = self._calculate_time_angles(date)
-        day_night_division = DayNightDivision.AM if date.hour < 12 else DayNightDivision.PM
+        hour_angle, minute_angle, second_angle = ClockTimeHelper.calculate_time_angles(date)
+        day_night_division = DayNightDivision.AM if date.hour < ClockConstants.HOURS_IN_HALF_DAY else DayNightDivision.PM
 
-        self.analog_clock.set_date_time(year, month, day, hour_angle, minute_angle, second_angle, day_night_division)
+        self._analog_clock.set_date_time(year, month, day, hour_angle, minute_angle, second_angle, day_night_division)
 
     def get_date_time(self) -> datetime:
         """
-        Получает текущее время с аналоговых часов
+        Получает текущее время с аналоговых часов, преобразуя углы времени в
+        часы, минуты и секунды, и возвращает их в формате datetime
         """
+        year = self._analog_clock.get_year()
+        month = self._analog_clock.get_month()
+        day = self._analog_clock.get_day()
 
-        year = self.analog_clock.get_year()
-        month = self.analog_clock.get_month()
-        day = self.analog_clock.get_day()
+        hour_angle = self._analog_clock.get_hour_angle()
+        minute_angle = self._analog_clock.get_minute_angle()
+        second_angle = self._analog_clock.get_second_angle()
 
-
-        hour_angle = self.analog_clock.get_hour_angle()
-        minute_angle = self.analog_clock.get_minute_angle()
-        second_angle = self.analog_clock.get_second_angle()
-
-        hour, minute, second = self._calculate_time_from_angles(hour_angle, minute_angle, second_angle)
+        hour, minute, second = ClockTimeHelper.calculate_time_from_angles(hour_angle, minute_angle, second_angle)
+        hour = ClockTimeHelper.adjust_for_day_night(hour, self._analog_clock.day_night_division)
 
         return datetime(year, month, day, hour, minute, second)
-
-    def _calculate_time_angles(self, date: datetime) -> tuple[float, float, float]:
-        """
-        Вычисляет углы для часов, минут и секунд на основе текущего времени
-
-        date - Дата и время в формате datetime
-
-        """
-        hour_angle = (date.hour % 12) * ClockConstants.HOUR_ANGLE_PER_HOUR + (date.minute / 60) * ClockConstants.HOUR_ANGLE_PER_HOUR
-        minute_angle = date.minute * ClockConstants.MINUTE_ANGLE_PER_MINUTE
-        second_angle = date.second * ClockConstants.SECOND_ANGLE_PER_SECOND
-        return hour_angle, minute_angle, second_angle
-
-    def _calculate_time_from_angles(self, hour_angle: float, minute_angle: float, second_angle: float) -> tuple[int, int, int]:
-        """
-        Вычисление времени на основе углов
-
-        hour_angle - Угол для часов
-        minute_angle - Угол для минут
-        second_angle - Угол для секунд
-        """
-        hour = int(hour_angle // ClockConstants.HOUR_ANGLE_PER_HOUR) % 12
-        minute = int(minute_angle // ClockConstants.MINUTE_ANGLE_PER_MINUTE)
-        second = int(second_angle // ClockConstants.SECOND_ANGLE_PER_SECOND)
-
-        if self.analog_clock.day_night_division == DayNightDivision.PM and hour != 12:
-            hour += 12
-        elif self.analog_clock.day_night_division == DayNightDivision.AM and hour == 12:
-            hour = 0
-
-        return hour, minute, second
