549a43f4a8f6500655ed166fd30f104159ae7fc1
Iutin12 <79744000+Iutin12@users.noreply.github.com>
2024-11-06 22:50:37 +0500

    fix: добавлены константы

--- consts/translate.py
+++ consts/translate.py
@@ -3,7 +3,13 @@ class ClockConstants:
     """
     Класс для хранения констант, связанных с часами
     """
-    HOUR_ANGLE_PER_HOUR = 30  # 360 (градусов)/ 12 часов
-    MINUTE_ANGLE_PER_MINUTE = 6  # 360 (градусов)/ 60 минут
-    SECOND_ANGLE_PER_SECOND = 6  # 360 (градусов)/ 60 секунд
+    # Время
     HOURS_IN_DAY = 24
+    HOURS_IN_HALF_DAY = 12  # Часы в половине суток (AM/PM)
+    SECONDS_IN_MINUTE = 60
+    MINUTES_IN_HOUR = 60
+
+    # Углы
+    HOUR_ANGLE_PER_HOUR = 360 / HOURS_IN_HALF_DAY  # Угол для одного часа
+    MINUTE_ANGLE_PER_MINUTE = 360 / SECONDS_IN_MINUTE  # Угол для одной минуты
+    SECOND_ANGLE_PER_SECOND = 360 / SECONDS_IN_MINUTE  # Угол для одной секунды
