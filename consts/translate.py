3da5ab69ec15c7e24bbc91f3628faf811715acda
Iutin12 <79744000+Iutin12@users.noreply.github.com>
2024-11-06 17:44:13 +0500

    feat: добавлены константы для адаптера

---
+++ consts/translate.py
@@ -0,0 +1,9 @@
+
+class ClockConstants:
+    """
+    Класс для хранения констант, связанных с часами
+    """
+    HOUR_ANGLE_PER_HOUR = 30  # 360 (градусов)/ 12 часов
+    MINUTE_ANGLE_PER_MINUTE = 6  # 360 (градусов)/ 60 минут
+    SECOND_ANGLE_PER_SECOND = 6  # 360 (градусов)/ 60 секунд
+    HOURS_IN_DAY = 24
