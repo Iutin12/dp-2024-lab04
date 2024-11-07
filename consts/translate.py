e6464cc2ba51cbf2b3aec78038c88255fd0683b5
Iutin12 <79744000+Iutin12@users.noreply.github.com>
2024-11-07 10:52:40 +0500

    fix: убраны магические числа

--- consts/translate.py
+++ consts/translate.py
@@ -8,8 +8,9 @@ class ClockConstants:
     HOURS_IN_HALF_DAY = 12  # Часы в половине суток (AM/PM)
     SECONDS_IN_MINUTE = 60
     MINUTES_IN_HOUR = 60
+    DEGREES = 360 # Всего градусов
 
     # Углы
-    HOUR_ANGLE_PER_HOUR = 360 / HOURS_IN_HALF_DAY  # Угол для одного часа
-    MINUTE_ANGLE_PER_MINUTE = 360 / SECONDS_IN_MINUTE  # Угол для одной минуты
-    SECOND_ANGLE_PER_SECOND = 360 / SECONDS_IN_MINUTE  # Угол для одной секунды
+    HOUR_ANGLE_PER_HOUR = DEGREES / HOURS_IN_HALF_DAY  # Угол для одного часа
+    MINUTE_ANGLE_PER_MINUTE = DEGREES / SECONDS_IN_MINUTE  # Угол для одной минуты
+    SECOND_ANGLE_PER_SECOND = DEGREES / SECONDS_IN_MINUTE  # Угол для одной секунды
