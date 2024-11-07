87f4bd777aaa520551f0833a402806638d67ad05
Iutin12 <79744000+Iutin12@users.noreply.github.com>
2024-11-07 10:48:24 +0500

    fix: добавлены датаклассы

--- clocks/clock_adapter.py
+++ clocks/clock_adapter.py
@@ -5,33 +5,44 @@ from interfaces.base_digital_clock import BaseDigitalClock
 from consts.translate import ClockConstants
 from clocks.helper import ClockTimeHelper
 
+
 class AnalogClockAdapter(BaseDigitalClock):
     def __init__(self, analog_clock: BaseAnalogClock) -> None:
         """
-        Инициализирует адаптер для аналоговых часов
+        Инициализирует адаптер для аналоговых часов.
 
-        :param analog_clock: Экземпляр класса, реализующего интерфейс BaseAnalogClock, который будет адаптирован для использования как цифровые часы
+        :param analog_clock: Экземпляр класса, реализующего интерфейс BaseAnalogClock, который будет адаптирован для использования как цифровые часы.
         """
         self._analog_clock = analog_clock
 
     def set_date_time(self, date: datetime) -> None:
         """
         Устанавливает дату и время на аналоговых часах, преобразуя их в углы для
-        часов, минут и секунд, а также определяя AM/PM для правильного отображения
+        часов, минут и секунд, а также определяя AM/PM для правильного отображения.
 
         :param date: Дата и время, которые нужно установить на аналоговых часах
         """
+        
         year, month, day = date.year, date.month, date.day
-        hour_angle, minute_angle, second_angle = ClockTimeHelper.calculate_time_angles(date)
+
+        time_angles = ClockTimeHelper.calculate_time_angles(date)
+
         day_night_division = DayNightDivision.AM if date.hour < ClockConstants.HOURS_IN_HALF_DAY else DayNightDivision.PM
 
-        self._analog_clock.set_date_time(year, month, day, hour_angle, minute_angle, second_angle, day_night_division)
+        self._analog_clock.set_date_time(
+            year, month, day,
+            time_angles.hour_angle,
+            time_angles.minute_angle,
+            time_angles.second_angle,
+            day_night_division
+        )
 
     def get_date_time(self) -> datetime:
         """
         Получает текущее время с аналоговых часов, преобразуя углы времени в
-        часы, минуты и секунды, и возвращает их в формате datetime
+        часы, минуты и секунды, и возвращает их в формате datetime.
         """
+
         year = self._analog_clock.get_year()
         month = self._analog_clock.get_month()
         day = self._analog_clock.get_day()
@@ -40,7 +51,10 @@ class AnalogClockAdapter(BaseDigitalClock):
         minute_angle = self._analog_clock.get_minute_angle()
         second_angle = self._analog_clock.get_second_angle()
 
-        hour, minute, second = ClockTimeHelper.calculate_time_from_angles(hour_angle, minute_angle, second_angle)
-        hour = ClockTimeHelper.adjust_for_day_night(hour, self._analog_clock.day_night_division)
+        clock_time = ClockTimeHelper.calculate_time_from_angles(hour_angle, minute_angle, second_angle)
 
-        return datetime(year, month, day, hour, minute, second)
+        hour = ClockTimeHelper.adjust_for_day_night(clock_time.hour, self._analog_clock.day_night_division)
+
+        return datetime(year, month, day, hour, clock_time.minute, clock_time.second)
+
+
